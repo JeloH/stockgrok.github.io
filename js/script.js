0f7aff430135aba1d9619bc56dd85a758fc29e0d
“Emily <essaltz@gmail.com>
2017-07-18 13:45:11 -0400

    svg!

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$("#play-group").focus(function() {
+  window.scrollTo(250,350);
+});
+$("#wakeup-group").focus(function(){
+  window.scrollTo(250 , 600);
+});
+$("#sleep-group" ).focus(function(){
+  window.scrollTo(250 , 850);
+});
+$("#feed-group" ).focus(function() {
+  window.scrollTo(250 , 1100);
+});
+$("#cuddle-group" ).focus(function(){
+  window.scrollTo(250 , 1350);
+});
+
